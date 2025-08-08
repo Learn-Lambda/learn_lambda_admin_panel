4da655df729037b06ac3d94898d106694d92d9d3
IDONTSUDO <idontsudo@gmail.com>
2025-08-08 13:35:59 +0300

    progress

--- src/core/ui/button/button.tsx
+++ src/core/ui/button/button.tsx
@@ -3,7 +3,6 @@ import { TextV2 } from "../text/text";
 
 export const Button: React.FC<{
   text?: string;
-<<<<<<< HEAD
   color?: string;
   height?: number;
   width?: number;
@@ -23,54 +22,3 @@ export const Button: React.FC<{
     <TextV2 text={text} color="white" />
   </div>
 );
-=======
-  onClick?: any;
-  textStyle?: React.CSSProperties;
-  style?: React.CSSProperties;
-}) {
-  return (
-    <div
-      onClick={() => props.onClick?.call()}
-      style={Object.assign(
-        {
-          width: "max-content",
-          backgroundColor: props.filled ? "rgba(103, 80, 164, 1)" : "",
-          paddingRight: 10,
-          paddingLeft: 10,
-          paddingTop: 10,
-          paddingBottom: 10,
-          borderRadius: 24,
-          border: props.block
-            ? "1px solid  rgba(29, 27, 32, 0.12)"
-            : props.filled
-            ? ""
-            : "1px solid black",
-          height: 50,
-        },
-        props.style
-      )}
-    >
-      <CoreText
-        text={props.text ?? ""}
-        type={CoreTextType.medium}
-        style={Object.assign(
-          {
-            fontSize: 20,
-            height: "100%",
-            alignContent: "center",
-            justifySelf: "center",
-          },
-          props.textStyle
-        )}
-        color={
-          props.block
-            ? "#1D1B20"
-            : props.filled
-            ? "white"
-            : "rgba(103, 80, 164, 1)"
-        }
-      />
-    </div>
-  );
-}
->>>>>>> 7595301ad1a6a6b52f5f60e85e6cc9ab5feae627
